986f068c1186e26b365edcc57a26c0eadb2d9e8b
ChoonXiangg <yeohcx192@gmail.com>
2025-09-26 13:34:39 +0100

    Added update_user, delete_user, and get_health in main.py. Added UserUpdate in schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,18 @@
 from pydantic import BaseModel, EmailStr, constr, conint
+from typing import Optional
 
 class User(BaseModel):
     user_id: int
     name: constr(min_length=2, max_length=50)
     email: EmailStr
     age: conint(gt=18)
+    student_id: constr(pattern=r'^S\d{7}$')
+
+#I want to allow users to only write the name and value they want to update instead of every name and value so I created a new class
+#For example, user_id: Optional[int] = None means if user_id is not in the request body, it will be None
+class UserUpdate(BaseModel):
+    user_id: Optional[int] = None
+    name: Optional[constr(min_length=2, max_length=50)] = None
+    email: Optional[EmailStr] = None
+    age: Optional[conint(gt=18)] = None
+    student_id: Optional[constr(pattern=r'^S\d{7}$')] = None
